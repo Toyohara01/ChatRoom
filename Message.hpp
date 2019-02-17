dcdebd09d64a899a4bac61558c0c8b5cdefd926e
edmondm4 <47162149+edmondm4@users.noreply.github.com>
2019-02-16 17:08:38 -0700

    Update Message.hpp

--- Message.hpp
+++ Message.hpp
@@ -1,12 +1,18 @@
 #ifndef MESSAGE_HPP
 #define MESSAGE_HPP
+#include <ctime>
 
 class Message
 {
     private: 
+	string content;
+	time_t timestamp;
+	string userID;
 
     public:
     Message();
+	parse();
+	process();
     ~Message();
 };
 
