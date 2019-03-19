4093f1da36cf1b9f3336853191df6ca0dd27539c
toyohara_01 <tristangogot@gmail.com>
2019-03-19 11:20:55 -0700

    Save and Loading

--- Archive.cpp
+++ Archive.cpp
@@ -30,7 +30,12 @@ void Archive::RAID()
 
 char* Archive::Xor(char *LBA1, char *LBA2, char *LBA3, char *LBA4, char *PLBA)
 {
-    
+	int idx;
+
+	for (idx = 0; idx < SECTOR_SIZE; idx++)
+	{
+		*(PLBA + idx) = (*(LBA1 + idx)) ^ (*(LBA2 + idx)) ^ (*(LBA3 + idx)) ^ (*(LBA4 + idx));
+	}
 }
 
 int main()
