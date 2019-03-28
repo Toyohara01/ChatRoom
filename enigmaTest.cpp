3f1c6a2a9ea834f28081a3a684ef2b7d0689aa64
delongl1 <47231997+delongl1@users.noreply.github.com>
2019-03-28 11:55:05 -0700

    In the process of updating the test driver (incomplete)

--- enigmaTest.cpp
+++ enigmaTest.cpp
@@ -10,16 +10,40 @@ int main()
     Enigma encryptObj;
     Enigma decryptObj;
 
-    //declare vairable for message
-    string text;
+    //declare vairables
+    string plainString, cipherString;
+    int testNum, i, ii, randIndex, randSize;
+    char characters[37] = {
+        'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K',
+        'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V',
+        'W', 'X', 'Y', 'Z', '0', '1', '2', '3', '4', '5', '6',
+        '7', '8', '9', ' '
+    };
 
     //prompt user for input
-    cout << "Enter Message: ";
-    getline(cin, text);
+    cout << "Enter desired number of test runs: ";
+    getline(cin, testNum);
 
-    //execute encyption method
-    text = encryptObj.Encrypt(text);
+    //loop to run desired number of test cases
+    for(i=0;i<testNum;i++)
+    {
+        //fill each element of the string with a random character 
+        for(ii=0;ii<256;ii++)
+        {
+            //generate random number
+            randIndex = rand()%38;
 
-    //execute decryption method
-    decryptObj.Decrypt(text);
+            plainString[ii] = characters[randIndex];
+
+        }
+
+        //print plaintext string generated
+        cout << plainString << endl;
+
+        //encrypt plain text string
+        cipherString = encryptObj.Encrypt(plainString);
+
+        //print out ciphertext string
+        cout << cipherString << endl;
+    }
 }
