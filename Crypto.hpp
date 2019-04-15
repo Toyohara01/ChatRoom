8e778ea6900110d8a23546dd10ef14dbfb2199ac
delongl1 <47231997+delongl1@users.noreply.github.com>
2019-04-15 13:07:10 -0700

    Moved declarations of rotors, reflector, and alphabet

--- Crypto.hpp
+++ Crypto.hpp
@@ -30,48 +30,12 @@ class Crypto
 class Enigma : public Crypto
 {
     private:
-        static constexpr char rotor1[26][2]=
-        {
-            {'A', 'E'}, {'B', 'K'}, {'C', 'M'}, {'D', 'F'},
-            {'E', 'L'}, {'F', 'G'}, {'G', 'D'}, {'H', 'Q'},
-            {'I', 'V'}, {'J', 'Z'}, {'K', 'N'}, {'L', 'T'},
-            {'M', 'O'}, {'N', 'W'}, {'O', 'Y'}, {'P', 'H'},
-            {'Q', 'X'}, {'R', 'U'}, {'S', 'S'}, {'T', 'P'},
-            {'U', 'A'}, {'V', 'I'}, {'W', 'B'}, {'X', 'R'},
-            {'Y', 'C'}, {'Z', 'J'}
-        };
-        static constexpr char rotor2[26][2]=
-        {
-            {'A', 'A'}, {'B', 'J'}, {'C', 'D'}, {'D', 'K'},
-            {'E', 'S'}, {'F', 'I'}, {'G', 'R'}, {'H', 'U'},
-            {'I', 'X'}, {'J', 'B'}, {'K', 'L'}, {'L', 'H'},
-            {'M', 'W'}, {'N', 'T'}, {'O', 'M'}, {'P', 'C'},
-            {'Q', 'Q'}, {'R', 'G'}, {'S', 'Z'}, {'T', 'N'},
-            {'U', 'P'}, {'V', 'Y'}, {'W', 'F'}, {'X', 'V'},
-            {'Y', 'O'}, {'Z', 'E'}
-        };
-        static constexpr char rotor3[26][2]=
-        {
-            {'A', 'B'}, {'B', 'D'}, {'C', 'F'}, {'D', 'H'},
-            {'E', 'J'}, {'F', 'L'}, {'G', 'C'}, {'H', 'P'},
-            {'I', 'R'}, {'J', 'T'}, {'K', 'X'}, {'L', 'V'},
-            {'M', 'Z'}, {'N', 'N'}, {'O', 'Y'}, {'P', 'E'},
-            {'Q', 'I'}, {'R', 'W'}, {'S', 'G'}, {'T', 'A'},
-            {'U', 'K'}, {'V', 'M'}, {'W', 'U'}, {'X', 'S'},
-            {'Y', 'Q'}, {'Z', 'O'}
-        };
-        static constexpr char alphabet[26]=
-        {
-            'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J',
-            'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T',
-            'U', 'V', 'W', 'X', 'Y', 'Z'
-        };
-        static constexpr char reflector[26]=
-        {
-            'A', 'B', 'C', 'D', 'E', 'F', 'G', 'D', 'I', 'J',
-            'K', 'G', 'M', 'K', 'M', 'I', 'E', 'B', 'F', 'T',
-            'C', 'V', 'V', 'J', 'A', 'T'
-        };
+        static char rotor1[26][2];
+        static char rotor2[26][2];
+        static char rotor3[26][2];
+        static char alphabet[26];
+        static char reflector[26];
+
         unsigned int key[3];
         std::string Encrypt(std::string input);
         std::string Decrypt(std::string input);
